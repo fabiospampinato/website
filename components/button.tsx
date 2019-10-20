db45142ab0af50905c8369c53471a15091106775
Fabio Spampinato <spampinabio@gmail.com>
2019-10-20 20:52:12 +0200

    Button: added support for a title

--- components/button.tsx
+++ components/button.tsx
@@ -5,8 +5,8 @@ import * as React from 'react';
 
 /* BUTTON */
 
-const Button = ({ className, href, icon }: { className?: string, href: string, icon: string }) => (
-  <a className={`button ${className || ''}`} href={href}>
+const Button = ({ className, href, icon, title }: { className?: string, href: string, icon: string, title: string }) => (
+  <a className={`button ${className || ''}`} href={href} title={title}>
     <img src={`/static/icons/${icon}.svg`} alt={icon} width="24" />
   </a>
 );
