b0051d5b0cbabbe6172b8baae003b296882451a7
Fabio Spampinato <spampinabio@gmail.com>
2020-04-23 11:50:40 +0100

    Thanks: updated

--- pages/thanks.tsx
+++ pages/thanks.tsx
@@ -13,18 +13,18 @@ const Thanks = () => (
     <p>You can support me, and have your name listed in this page, via <a href="https://github.com/sponsors/fabiospampinato" title="Sponsor me on GitHub">GitHub</a>. Alternatively I also accept direct one-time donations via <a href="https://www.paypal.me/fabiospampinato" title="Sponsor me on PayPal">PayPal</a>.</p>
     <h2>Super Supporters <span className="price">$25/month</span></h2>
     <ul>
-      <li className="empty">No super supporters yet.</li>
+      <li><a href="https://github.com/ramonza">Ramon Nogueira</a></li>
     </ul>
     <h2>Top Supporters <span className="price">$10/month</span></h2>
     <ul>
       <li><a href="https://github.com/bstinsonmhk">Brian Stinson</a></li>
       <li><a href="https://github.com/carlodicelico">Carlo DiCelico</a></li>
       <li><a href="https://github.com/feipenghe">Pengfei He</a></li>
+      <li><a href="https://github.com/nkaleidoskop">nkaleidoskop</a></li>
     </ul>
     <h2>Supporters <span className="price">$5/month</span></h2>
     <ul>
       <li><a href="https://github.com/limonte">Limon Monte</a></li>
-      <li><a href="https://github.com/ddhogan">Donna Hogan</a></li>
       <li><a href="https://github.com/BobMilli">Bob Milli</a></li>
       <li><a href="https://github.com/pinklux">pinklux</a></li>
       <li><a href="https://github.com/ernstki">Kevin Ernst</a></li>
@@ -32,7 +32,6 @@ const Thanks = () => (
       <li><a href="https://github.com/rdipietro">Robert DiPietro</a></li>
       <li><a href="https://github.com/tkilaker">Tim Kilåker</a></li>
       <li><a href="https://github.com/esai256">Elias Seichter</a></li>
-      <li><a href="https://github.com/thomastu">Thomas Tu</a></li>
       <li><a href="https://github.com/jhconning">Jonathan Conning</a></li>
       <li><a href="https://github.com/kyldvs">Kyle Davis</a></li>
       <li><a href="https://github.com/epmoyer">Eric Moyer</a></li>
