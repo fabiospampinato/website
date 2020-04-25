eb00aa2a1a71f354f994d0f46a4eef782997d20c
Fabio Spampinato <spampinabio@gmail.com>
2020-04-25 23:45:01 +0100

    Thanks: updated

--- pages/thanks.tsx
+++ pages/thanks.tsx
@@ -45,6 +45,7 @@ const Thanks = () => (
       <li><a href="https://github.com/da-moon">da-moon</a></li>
       <li><a href="https://github.com/CBornes">Carlos Bornes</a></li>
       <li><a href="https://github.com/JoshCooley">Josh Cooley</a></li>
+      <li><a href="https://github.com/skostojohn">Scott Kostojohn</a></li>
     </ul>
   </Layout>
 );
